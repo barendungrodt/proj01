ee9fca1dfc63ba5c10a04bcfc6b666ef0bc60908
njd911 <njdhockeydude@gmail.com>
2014-01-15 13:07:39 -0500

    created constructor in student header file

--- proj01/student.h
+++ proj01/student.h
@@ -9,6 +9,28 @@
 #ifndef proj01_student_h
 #define proj01_student_h
 
-
+class Student
+{
+public:
+    
+    /*
+     *  condtructor for student
+     */
+    Student(int studentID = 0, string name = "", int year = 0, float grade = 0.0)
+    {
+        m_studentID = studentID;
+        m_name = name;
+        m_year = year;
+        m_grade = grade;
+    }
+    
+    
+protected:
+    
+    int m_studentID;
+    string m_name;
+    int m_year;
+    float m_grade; // GPA
+};
 
 #endif
