1e0386b8219a4b8276000b95e96563521a61ce30
njd911 <njdhockeydude@gmail.com>
2014-01-15 12:00:12 -0500

    added student.h

---
+++ proj01/student.h
@@ -0,0 +1,14 @@
+//
+//  student.h
+//  proj01
+//
+//  Created by Nick Durak on 1/15/14.
+//  Copyright (c) 2014 Barend Ungrodt. All rights reserved.
+//
+
+#ifndef proj01_student_h
+#define proj01_student_h
+
+
+
+#endif
